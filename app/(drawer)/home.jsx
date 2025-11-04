fd2e1c73bac3fd73279c4d175f66ff93222387fe
Tesa44 <mateusz.tesarewicz123@gmail.com>
2025-11-04 19:51:30 +0100

    apply ScreenLayout to home page

--- app/(drawer)/home.jsx
+++ app/(drawer)/home.jsx
@@ -1,6 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
+import ScreenLayout from "../../components/layout/ScreenLayout";
 import BurgerMenuButton from "../../components/ui/BurgerMenuButton";
 import Header from "../../components/ui/Header";
 import HomeTileButton from "../../components/ui/HomeTileButton";
@@ -10,34 +11,36 @@ export default function Home() {
   const navigation = useNavigation();
 
   return (
-    <View style={styles.container}>
-      <View style={styles.burgerArea}>
-        <BurgerMenuButton onPress={() => navigation.toggleDrawer()} />
-      </View>
-
-      <ScrollView contentContainerStyle={styles.mainContent}>
-        <Header title="What do you feel like learning today?" />
-
-        <View style={styles.tilesContainer}>
-          <HomeTileButton
-            title="Flags"
-            imageSource={require("../../assets/images/img_flags.png")}
-          />
-          <HomeTileButton
-            title="Maps"
-            imageSource={require("../../assets/images/img_maps.png")}
-          />
-          <HomeTileButton
-            title="Capitals"
-            imageSource={require("../../assets/images/img_capitals.png")}
-          />
-          <HomeTileButton
-            title="Try yourself in quizzes"
-            imageSource={require("../../assets/images/img_quizzes.png")}
-          />
+    <ScreenLayout>
+      <View style={styles.container}>
+        <View style={styles.burgerArea}>
+          <BurgerMenuButton onPress={() => navigation.toggleDrawer()} />
         </View>
-      </ScrollView>
-    </View>
+
+        <ScrollView contentContainerStyle={styles.mainContent}>
+          <Header title="What do you feel like learning today?" />
+
+          <View style={styles.tilesContainer}>
+            <HomeTileButton
+              title="Flags"
+              imageSource={require("../../assets/images/img_flags.png")}
+            />
+            <HomeTileButton
+              title="Maps"
+              imageSource={require("../../assets/images/img_maps.png")}
+            />
+            <HomeTileButton
+              title="Capitals"
+              imageSource={require("../../assets/images/img_capitals.png")}
+            />
+            <HomeTileButton
+              title="Try yourself in quizzes"
+              imageSource={require("../../assets/images/img_quizzes.png")}
+            />
+          </View>
+        </ScrollView>
+      </View>
+    </ScreenLayout>
   );
 }
 
@@ -46,23 +49,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Colors.primaryLight,
     alignItems: "center",
-    paddingTop: 48,
   },
 
   burgerArea: {
     position: "absolute",
-    top: 65,
+    top: 5,
     left: 13.5,
     zIndex: 10,
   },
 
   mainContent: {
     flex: 1,
-    width: 280,
+    width: "80%",
     height: "100%",
     justifyContent: "space-between",
-    paddingTop: "4%",
-    paddingBottom: "11%",
   },
 
   tilesContainer: {
